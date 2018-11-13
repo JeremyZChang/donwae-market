08618559a10bf8e7c11f11471a9932b0202ff601
zhangtianyu <zhangtianyu39124@goldwind.com.cn>
2018-11-13 10:39:25 +0800

    Add Apache Shiro to Pom/ Add SpringBeanFactoryUtils

---
+++ market-core/src/main/java/com/donwae/market/utils/SpringBeanFactoryUtils.java
@@ -0,0 +1,28 @@
+package com.donwae.market.utils;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * SpringBeanFactory Util
+ * @auther Jeremy
+ * 2018/11/13 上午10:27
+ */
+@Component
+public class SpringBeanFactoryUtils implements ApplicationContextAware {
+
+    private static ApplicationContext context = null;
+
+    public static <T> T getBean(Class<T> type) {
+        return context.getBean(type);
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        if (SpringBeanFactoryUtils.context == null) {
+            SpringBeanFactoryUtils.context = applicationContext;
+        }
+    }
+}
