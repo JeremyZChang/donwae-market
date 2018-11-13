93042d049c09b661124b465d6beb702f1f06b8e4
zhangtianyu <zhangtianyu39124@goldwind.com.cn>
2018-11-13 10:51:03 +0800

    modify login controller to user controller

--- market-muskmelon/src/main/java/com/donwae/market/melon/controller/UserController.java
+++ market-muskmelon/src/main/java/com/donwae/market/melon/controller/UserController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/user")
-public class LoginController {
+public class UserController {
 
     @Autowired
     private UserService userService;
